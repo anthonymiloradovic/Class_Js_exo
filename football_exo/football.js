6a7bbec13c66212e25c666a28c60cf41f14d91fa
anthonymiloradovic <anthony.miloradovic@icloud.com>
2023-01-15 21:48:37 +0100

    player-select option

--- football_exo/football.js
+++ football_exo/football.js
@@ -30,14 +30,14 @@ attacker1.innerHTML = attackers[3].name;
 let attacker2 = document.getElementById("attacker-2");
 attacker2.innerHTML = attackers[1].name;
 let attacker3 = document.getElementById("attacker-3");
-attacker3.innerHTML = attackers[0].name;
+attacker3.innerHTML = attackers[6].name;
 
 let midfielder1 = document.getElementById("midfielder-1");
 midfielder1.innerHTML = midfielders[0].name;
 let midfielder2 = document.getElementById("midfielder-2");
 midfielder2.innerHTML = midfielders[1].name;
 let midfielder3 = document.getElementById("midfielder-3");
-midfielder3.innerHTML = midfielders[2].name;
+midfielder3.innerHTML = midfielders[9].name;
 
 let defender1 = document.getElementById("defender-1");
 defender1.innerHTML = defenders[14].name;
@@ -46,26 +46,40 @@ defender2.innerHTML = defenders[1].name;
 let defender3 = document.getElementById("defender-3");
 defender3.innerHTML = defenders[0].name;
 let defender4 = document.getElementById("defender-4");
-defender4.innerHTML = defenders[9].name;
+defender4.innerHTML = defenders[6].name;
 
 let goalkeeper = document.getElementById("goalkeeper-1");
 goalkeeper.innerHTML = goalkeepers[2].name;
 
-let select = document.getElementById("players-select");
-
-select.addEventListener("change", function() {
-    updatePlayer(select.value, attackers, "attacker-1");
-    updatePlayer(select.value+1, attackers, "attacker-2");
-    updatePlayer(select.value+2, attackers, "attacker-3");
-});
-
 function updatePlayer(selectedPlayerId, players, elementId) {
-    // vérifier si l'élément ciblé existe dans la page HTML
     const element = document.getElementById(elementId);
-    if (!element) return;
-
-    // vérifier si l'index sélectionné existe bien dans le tableau
-    if(players[selectedPlayerId]){
+    if(players[selectedPlayerId] && players[selectedPlayerId].name){
         element.innerHTML = players[selectedPlayerId].name;
     }
 }
+let select = document.getElementById("players-select");
+let select2 = document.getElementById("players-select-2");
+let select3 = document.getElementById("players-select-3");
+let select4 = document.getElementById("players-select-4");
+
+select.addEventListener("change", function() {
+    updatePlayer(select.value, attackers, "attacker-1");
+    updatePlayer(select.value + 4, attackers, "attacker-1");
+    updatePlayer(select.value + 2, attackers, "attacker-1");
+});
+
+select2.addEventListener("change", function() {
+    if(this === select2) {
+        updatePlayer(select2.value, midfielders, "midfielder-2");
+    }
+});
+select3.addEventListener("change", function() {
+    if(this === select3) {
+        updatePlayer(select3.value, defenders, "defender-3");
+    }
+});
+select4.addEventListener("change", function() {
+    if(this === select4) {
+        updatePlayer(select4.value, goalkeepers, "goalkeeper-1");
+    }
+});
